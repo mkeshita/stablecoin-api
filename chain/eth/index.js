4d066bc1a09abb4ac7b3585eb47da24a8ae37b5c
v1rtl <pilll.pl22@gmail.com>
2021-03-20 00:24:39 +0300

    add support for api keys

--- chain/eth/index.js
+++ chain/eth/index.js
@@ -2,28 +2,33 @@ import * as ethers from 'ethers'
 import ngnt from '../../coins/ngnt.js'
 import usdc from '../../coins/usdc.js'
 
-const provider = ethers.getDefaultProvider()
-const ngntContract = new ethers.Contract(ngnt.contractAddress, ngnt.abi, provider)
-const usdcContract = new ethers.Contract(usdc.contractAddress, usdc.abi, provider)
-
 export default {
-  async balance(address, coin) {
+  async balance(address, coin, apiKeys) {
+    const provider = ethers.getDefaultProvider(
+      'homestead',
+      apiKeys
+        ? {
+            etherscan: apiKeys.etherscan,
+            infura: apiKeys.infura,
+            alchemy: apiKeys.alchemy,
+            pocket: apiKeys.pocket
+          }
+        : undefined
+    )
+    const ngntContract = new ethers.Contract(ngnt.contractAddress, ngnt.abi, provider)
+    const usdcContract = new ethers.Contract(usdc.contractAddress, usdc.abi, provider)
+
+    function getContract(coin) {
+      if (coin === 'NGNT') return ngntContract
+      else if (coin === 'USDC') return usdcContract
+      else throw 'Invalid Coin Passed'
+    }
+
     if (!coin) throw 'Select either NGNT or USDC as your coin to query.'
     let contract = getContract(coin)
 
-    try {
-      let res = await contract.balanceOf(address)
+    let res = await contract.balanceOf(address)
 
-      return res.toString()
-    } catch (err) {
-      console.log(err)
-      return err
-    }
+    return res.toString()
   }
-}
-
-function getContract(coin) {
-  if (coin === 'NGNT') return ngntContract
-  else if (coin === 'USDC') return usdcContract
-  else throw 'Invalid Coin Passed'
 }
--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import eth from './chain/eth/index.js'
 
-export const balance = (address, coin) => {
+/**
+ *
+ * @param {string} address
+ * @param {string} coin
+ * @param {Record<string, string>} apiKeys
+ */
+export const balance = (address, coin, apiKeys) => {
   if (!address) throw 'Please send an address to query balance for.'
-  else if (coin === 'NGNT' || coin === 'USDC') return eth.balance(address, coin)
-  else if (address.slice(0, 2) === '0x') return eth.balance(address, coin)
+  else if (coin === 'NGNT' || coin === 'USDC') return eth.balance(address, coin, apiKeys)
+  else if (address.slice(0, 2) === '0x') return eth.balance(address, coin, apiKeys)
   else throw new Error('Invalid Coin Passed. Supported coins are NGNT and USDC.')
 }
